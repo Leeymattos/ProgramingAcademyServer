a9ae906c4d4e9ce6bab67fae36a8f595fede9af4
weslley matos <weslley_mattosibf@hotmail.com>
2022-11-01 11:45:27 -0300

    feat: added deploy configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,14 +8,25 @@ import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   port: Number(process.env.DB_PORT),
+    //   username: process.env.DB_USERNAME,
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_DATABASE,
+    //   entities: [__dirname + '/**/*.entity{.js,.ts}'],
+    //   synchronize: true
+    // }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [__dirname + '/**/*.entity{.js,.ts}'],
-      synchronize: true
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      logging: false,
+      dropSchema: false,
+      ssl: {
+        rejectUnauthorized: false
+      },
+      synchronize: true,
+      autoLoadEntities: true
     }),
     VideoModule,
     UserModule,
